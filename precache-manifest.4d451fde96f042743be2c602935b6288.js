e2bea6222834820cc2e68cbf3c07f200417e1ba5
KingZak28 <zakariah28@hotmail.com>
2019-09-09 23:07:45 -0400

    Updates

---
+++ precache-manifest.4d451fde96f042743be2c602935b6288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f40488125441cbc9112026d33247a0c",
+    "url": "/characterSelect/index.html"
+  },
+  {
+    "revision": "6bf893918d7aa225d9a7",
+    "url": "/characterSelect/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ea1e1b30d9c264c6cc13",
+    "url": "/characterSelect/static/css/main.fc66ef10.chunk.css"
+  },
+  {
+    "revision": "6bf893918d7aa225d9a7",
+    "url": "/characterSelect/static/js/2.0ba8849b.chunk.js"
+  },
+  {
+    "revision": "ea1e1b30d9c264c6cc13",
+    "url": "/characterSelect/static/js/main.ebfed7d5.chunk.js"
+  },
+  {
+    "revision": "8279681141193995881a",
+    "url": "/characterSelect/static/js/runtime~main.9cd146b8.js"
+  },
+  {
+    "revision": "82848488b73a87d72f2436c02b9ab608",
+    "url": "/characterSelect/static/media/one piece font.82848488.ttf"
+  }
+]);
